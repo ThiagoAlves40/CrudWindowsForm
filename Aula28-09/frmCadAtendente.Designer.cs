2ee40802e4fe6b8406ed3809b0bacda393edddac
Thiago.Alves <thiago.alves@castgroup.com.br>
2021-09-30 10:52:07 -0300

    Adicionar arquivos de projeto.

---
+++ Aula28-09/frmCadAtendente.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace Aula28_09
+{
+    partial class frmCadAtendente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvfunc = new System.Windows.Forms.DataGridView();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTestaConexao = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblCod = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvfunc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvfunc
+            // 
+            this.dgvfunc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvfunc.Location = new System.Drawing.Point(46, 283);
+            this.dgvfunc.Name = "dgvfunc";
+            this.dgvfunc.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvfunc.Size = new System.Drawing.Size(596, 131);
+            this.dgvfunc.TabIndex = 29;
+            this.dgvfunc.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dgvfunc_MouseDoubleClick);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(528, 185);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(114, 33);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(528, 143);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(114, 33);
+            this.btnRemover.TabIndex = 6;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlterar.Location = new System.Drawing.Point(528, 101);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(114, 33);
+            this.btnAlterar.TabIndex = 5;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(528, 57);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(114, 33);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovo.Location = new System.Drawing.Point(528, 16);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(114, 32);
+            this.btnNovo.TabIndex = 3;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 25);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Buscar";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(120, 234);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(522, 31);
+            this.txtBuscar.TabIndex = 10;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(120, 150);
+            this.txtSenha.MaxLength = 8;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(368, 31);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogin.Location = new System.Drawing.Point(120, 109);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(368, 31);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(120, 65);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(368, 31);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 25);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Senha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Login";
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nome.Location = new System.Drawing.Point(36, 68);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(68, 25);
+            this.Nome.TabIndex = 16;
+            this.Nome.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(41, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Funcionario";
+            // 
+            // btnTestaConexao
+            // 
+            this.btnTestaConexao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTestaConexao.Location = new System.Drawing.Point(224, 16);
+            this.btnTestaConexao.Name = "btnTestaConexao";
+            this.btnTestaConexao.Size = new System.Drawing.Size(114, 32);
+            this.btnTestaConexao.TabIndex = 9;
+            this.btnTestaConexao.Text = "Teste";
+            this.btnTestaConexao.UseVisualStyleBackColor = true;
+            this.btnTestaConexao.Click += new System.EventHandler(this.btnTestaConexao_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(374, 16);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(114, 32);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lblCod
+            // 
+            this.lblCod.AutoSize = true;
+            this.lblCod.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCod.Location = new System.Drawing.Point(286, 193);
+            this.lblCod.Name = "lblCod";
+            this.lblCod.Size = new System.Drawing.Size(80, 25);
+            this.lblCod.TabIndex = 32;
+            this.lblCod.Text = "Código";
+            // 
+            // frmCadAtendente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 439);
+            this.Controls.Add(this.lblCod);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnTestaConexao);
+            this.Controls.Add(this.dgvfunc);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCadAtendente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmCadAtendente";
+            this.Load += new System.EventHandler(this.frmCadAtendente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvfunc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvfunc;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTestaConexao;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblCod;
+    }
+}
---
+++ Aula28-09/frmCadAtendente.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Aula28_09
+{
+    public partial class frmCadAtendente : Form
+    {
+        public frmCadAtendente()
+        {
+            InitializeComponent();
+        }
+
+        private void limparTextBoxes(Control.ControlCollection controles)
+        {
+            //Faz um laço para todos os controles passados no parâmetro
+            foreach (Control ctrl in controles)
+            {
+                //Se o contorle for um TextBox...
+                if (ctrl is TextBox)
+                {
+                    ((TextBox)(ctrl)).Text = String.Empty;
+                }
+            }
+        }
+
+        private void desabilitaCampos()
+        {
+            txtNome.Enabled = false;
+            txtLogin.Enabled = false;
+            txtSenha.Enabled = false;
+            btnAlterar.Enabled = false;
+            btnCancelar.Enabled = false;
+            btnRemover.Enabled = false;
+            btnSalvar.Enabled = false;
+        }
+
+        private void habilitarCampos()
+        {
+            txtNome.Enabled = true;
+            txtLogin.Enabled = true;
+            txtSenha.Enabled = true;
+            txtNome.Focus();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            habilitarCampos();
+            btnSalvar.Enabled = true;
+            btnNovo.Enabled = false;
+        }
+
+        private void btnTestaConexao_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = conexao.obterConexao();
+
+            // a conexão foi efetuada com sucesso?
+            if (conn == null)
+            {
+                MessageBox.Show("Não foi possível obter a conexão. Veja o log de erros.");
+            }
+
+            else
+            {
+                MessageBox.Show("A conexão foi obtida com sucesso.");
+            }
+
+            // não precisamos mais da conexão? vamos fechá-la
+            conexao.fecharConexao();
+        }
+
+        private void frmCadAtendente_Load(object sender, EventArgs e)
+        {
+            desabilitaCampos();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = conexao.obterConexao();
+            SqlCommand objComandoSql = new SqlCommand();
+
+            objComandoSql.Connection = conn;
+
+            btnNovo.Enabled = false;
+
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Campo nome é obrigatório");
+                txtNome.Focus();
+            }
+
+            else if (txtLogin.Text == "")
+            {
+                MessageBox.Show("Campo login é obrigatório");
+                txtLogin.Focus();
+            }
+
+            else if (txtSenha.Text == "")
+            {
+                MessageBox.Show("Campo senha é obrigatório");
+                txtSenha.Focus();
+            }
+
+            else
+            {
+                try
+                {
+                    string login = txtLogin.Text;
+                    string nome = txtNome.Text;
+                    string senha = txtSenha.Text;
+
+                    string strSql = $"insert into tbl_atendente (ds_login,ds_senha,nm_atendente) values ('{login}','{senha}','{nome}')";
+
+                    objComandoSql = new SqlCommand(strSql, conn);
+                    //conn.Open();
+                    objComandoSql.ExecuteNonQuery();
+                }
+
+                catch (Exception erro)
+                {
+                    MessageBox.Show("" + erro);
+                    conn.Close();
+                }
+
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparTextBoxes(this.Controls);
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection conn = conexao.obterConexao();
+            SqlCommand objComandoSql = new SqlCommand();
+
+            objComandoSql.Connection = conn;
+            if (txtBuscar.Text != "")
+            {
+                try
+                {
+                    objComandoSql.CommandText = "Select * from tbl_atendente where nm_atendente like ('%" + txtBuscar.Text + "%')";
+                    objComandoSql.Connection = conn;
+
+                    //recebe os dados de uma tabela apos a execuçã de uma select
+                    SqlDataAdapter da = new SqlDataAdapter();
+
+                    DataTable dt = new DataTable();
+
+                    //recebe os dados da instrução select
+                    da.SelectCommand = objComandoSql;
+                    da.Fill(dt); //preenche o data table
+
+                    dgvfunc.DataSource = dt;
+                }
+
+                catch (Exception erro)
+                {
+                    MessageBox.Show("\n" + erro);
+                    conn.Close();
+                }
+            }
+
+            else
+            {
+                dgvfunc.DataSource = null;
+                conn.Close();
+            }
+        }
+
+        private void carregarAtendente()
+        {
+            lblCod.Text = dgvfunc.SelectedRows[0].Cells[0].Value.ToString();
+            txtLogin.Text = dgvfunc.SelectedRows[0].Cells[1].Value.ToString();
+            txtSenha.Text = dgvfunc.SelectedRows[0].Cells[2].Value.ToString();
+            txtNome.Text = dgvfunc.SelectedRows[0].Cells[3].Value.ToString();
+            habilitarCampos();
+            btnNovo.Enabled = false;
+            btnSalvar.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnRemover.Enabled = true;
+            btnCancelar.Enabled = true;
+            btnLimpar.Enabled = false;
+        }
+
+        private void dgvfunc_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            carregarAtendente();
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = conexao.obterConexao();
+            SqlCommand objComandoSql = new SqlCommand();
+
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Campo nome é obrigatório");
+                txtNome.Focus();
+            }
+
+            else if (txtLogin.Text == "")
+            {
+                MessageBox.Show("Login vazio");
+                txtLogin.Focus();
+            }
+
+            else if (txtSenha.Text == "" && txtSenha.Text.Length < 8)
+            {
+                MessageBox.Show("Senha vazio / menos de 8 caractres");
+                txtSenha.Focus();
+            }
+
+            else
+            {
+                try
+                {
+                    string login = txtLogin.Text;
+                    string senha = txtSenha.Text;
+                    string nome = txtNome.Text;
+                    int cd = Convert.ToInt32(lblCod.Text);
+
+                    string strSql = "update tbl_atendente set ds_login = @login,ds_senha=@senha,nm_atendente=@nome where cd_atendente=@cd";
+
+                    objComandoSql.CommandText = strSql;
+                    objComandoSql.Connection = conn;
+
+                    objComandoSql.Parameters.Add("@login", SqlDbType.VarChar).Value = login;
+                    objComandoSql.Parameters.Add("@senha", SqlDbType.Char).Value = senha;
+                    objComandoSql.Parameters.Add("@nome", SqlDbType.VarChar).Value = nome;
+                    objComandoSql.Parameters.Add("@cd", SqlDbType.Int).Value = cd;
+
+                    // conn.Open();
+                    objComandoSql.ExecuteNonQuery();
+                    objComandoSql.Parameters.Clear();
+
+                    MessageBox.Show("Dados alterados com sucesso");
+                    //limparCampos();
+                    dgvfunc.Refresh();
+                }
+
+                catch (Exception erro)
+                {
+                    MessageBox.Show("Algo deu Ruim\n" + erro.Message);
+                    conn.Close();
+                }
+
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = conexao.obterConexao();
+            SqlCommand objComandoSql = new SqlCommand();
+
+            try
+            {
+                if (DialogResult.OK == MessageBox.Show("Deseja remover funcionario?", "Remover",
+               MessageBoxButtons.OKCancel, MessageBoxIcon.Question))
+                {
+                    int cd = Convert.ToInt32(lblCod.Text);
+                    string strSql = $"delete from tbl_atendente where cd_atendente = {cd};";
+
+                    objComandoSql.CommandText = strSql;
+                    objComandoSql.Connection = conn;
+                    objComandoSql.ExecuteNonQuery();
+                }
+
+                else
+                {
+                    MessageBox.Show("Funcionario mantido");
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+                conn.Close();
+            }
+        }
+    }
+}
